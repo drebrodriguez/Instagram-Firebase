c1fd96cca08c50e2a54d087ee191c7d0211aaa04
Dreb Rodriguez <dreb.rodriguez@gmail.com>
2018-04-28 17:17:12 +0800

    [🚧] WIP Registration Screen [🛠] Refactor UIColor and Anchor [➕] Added Firebase framework [💄] Rework UI coloring and textfield behaviour
    #Update with Firebase integration for user registration
    #and added some alerts for conveniency to show
    #registration status and other minimal UI cleanup.
    #up next EP4-5 later today.

---
+++ workspace/lbta-instagram/Util/Extensions.swift
@@ -0,0 +1,44 @@
+//
+//  Extensions.swift
+//  lbta-instagram
+//
+//  Created by Dreb Rodriguez on 27/04/2018.
+//  Copyright © 2018 Joash Tubaga. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    static func rgbWithAlpha(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat, _ alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
+    }
+}
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?,paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
