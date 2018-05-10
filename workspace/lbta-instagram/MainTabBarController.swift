f40492dad7eb1208fd31f5b77b6609f4acd37384
Dreb Rodriguez <dreb.rodriguez@gmail.com>
2018-05-10 12:48:43 +0800

    [🚧] WIP Home Feed [🎉] Initial commit home feed [🐎] Optimized image caching [🍕][💄] New asset and UI
    #branch will not be deleted after merge.
    #next post details in home feed.

--- workspace/lbta-instagram/MainTabBarController.swift
+++ workspace/lbta-instagram/MainTabBarController.swift
@@ -42,7 +42,7 @@ class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
     }
     
     func setupViewControllers() {
-        let homeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"))
+        let homeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: HomeController(collectionViewLayout: UICollectionViewFlowLayout()))
         let searchNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"))
         let plusNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"))
         let likeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"))
